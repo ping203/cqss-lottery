dd7418b951787b98e9297f175a9a2e3ce2dd0997
linyng <linyngfly@gmail.com>
2017-05-25 13:23:19 +0800

    file upload

--- web-server/app.js
+++ web-server/app.js
@@ -1,10 +1,13 @@
 const Koa = require('koa');
 const app = new Koa();
 const views = require('koa-views');
+const static = require('koa-static');
 const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser')();
 const logger = require('koa-logger');
+const cors = require('koa-cors');
+const path = require('path');
 
 const mysql = require('./lib/dao/mysql/mysql');
 const daoUser = require('./lib/dao/daoUser');
@@ -12,16 +15,32 @@ const daoUser = require('./lib/dao/daoUser');
 const index = require('./routes/index');
 const users = require('./routes/users');
 
+// var mount_uploadify = require('koa-uploadify')
+//
+// mount_uploadify(app,{
+//     path:'/upload',
+//     fileKey:'myfile',
+//     multer:{ dest: 'uploads/' },
+//     callback:function(req){
+//         console.log('-----------------',req.files);
+//         return req.files
+//     }
+// });
+
+
 // error handler
 onerror(app);
 
 // middlewares
+//跨域访问
+//app.use(cors());
+app.use(logger());
 app.use(bodyparser);
 app.use(json());
-app.use(logger());
-app.use(require('koa-static')(__dirname + '/public'));
 
-app.use(views(__dirname + '/views', {
+app.use(static(path.join(__dirname, '/public')));
+
+app.use(views(path.join(__dirname , '/views'), {
   extension: 'ejs'
 }));
 
--- web-server/routes/index.js
+++ web-server/routes/index.js
@@ -1,4 +1,6 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
+const path = require('path');
+const { uploadFile } = require('../lib/upload')
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -9,13 +11,30 @@ router.get('/', async (ctx, next) => {
 
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
-})
+});
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
-})
+});
 
+router.get('/upload', async(ctx,next)=>{
+    let title = 'upload file async';
+    await ctx.render('upload',{title});
+});
+
+router.post('/upload', async(ctx,next)=>{
+    let result = { success: false }
+    let serverFilePath = path.join( __dirname, '..' );
+    let resourcePath = path.join(serverFilePath, 'public/uploads');
+
+    // 上传文件事件
+    result = await uploadFile( ctx, {
+        fileType: 'album',
+        path: resourcePath
+    })
+    ctx.body = result
+});
 
 module.exports = router
