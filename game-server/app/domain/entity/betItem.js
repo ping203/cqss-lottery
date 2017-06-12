138efa322d8bb01dd04187fe8078893280df7d53
林洋 <linyng@linyng-pc.local>
2017-06-12 22:19:12 +0800

    add

--- game-server/app/domain/entity/betItem.js
+++ game-server/app/domain/entity/betItem.js
@@ -85,7 +85,7 @@ BetItem.prototype.getIncomValue = function (openInfo, item) {
     var multi = this.incomeCfg.getBetRate(item.type.code);
 
     if(openInfo.has(item.result)){
-        inc = item.money * multi;
+        inc = item.money * (1+ multi);
     }
 
     if(inc > 0) this.winCount++;
