7884e9774abf44422050fdcb7c93d3d9f27f8b4b
linyngfly <linyngfly@163.com>
2017-06-26 17:52:35 +0800

    a

--- web-server/public/js/client.js
+++ web-server/public/js/client.js
@@ -141,6 +141,7 @@ $(document).ready(function () {
     var index = 0;
     function autoChat() {
         var route = "game.playerHandler.sendChatMsg";
+        index++;
         var msg = playerInfo.roleName + index + ":" +  "当圣火第一次点燃是希望在跟随，当终点已不再永久是心灵在体会，不在乎等待几多轮回，不在乎欢笑伴着泪水，超越梦想一起飞";
         pomelo.request(route, {
             from: playerInfo.roleName,
